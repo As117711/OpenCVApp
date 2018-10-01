25c84908501ec6cba0eca1faa78da55445f3e779
Adrian <adrian.knhumanoid@gmail.com>
2018-10-01 10:12:57 +0200

    Setting Up EnguCV - First Methods

--- OpenCVApp/OpenCVApp/Program.cs
+++ OpenCVApp/OpenCVApp/Program.cs
@@ -3,6 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Features2D;
+using Emgu.CV.XFeatures2D;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using Emgu.CV.Util;
+
 
 namespace OpenCVApp
 {
@@ -10,6 +18,12 @@ namespace OpenCVApp
     {
         static void Main(string[] args)
         {
+            using (var modelImage = new Image<Gray, byte>(@"model.jpg"))
+            {
+                var surfDetector = new SURF(400);
+                var ImageFeatures = surfDetector.Detect(modelImage, null);
+
+            }
         }
     }
 }
